c294f7b793412bc8f0a9d04ba255d7abff91b52a
trongvami <nluductrong@gmail.com>
2023-07-19 05:25:01 +0700

    asd

---
+++ Demo/Asp Net Core Web API EF 5/Source Code/MyFirstWebAPI/MyFirstWebAPI/Controllers/HangHoaController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hanghoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll() {
+            return Ok(hanghoas);
+        }
+
+        [HttpGet("{guid}")]
+        public IActionResult GetById(string guid)
+        {
+            try {
+                var hh = hanghoas.SingleOrDefault(x => x.HangHoaId == Guid.Parse(guid));
+                if (hh == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(hh);
+                }
+            }
+            catch (Exception ex) {
+                return BadRequest();
+            }
+            
+        }
+
+        [HttpPost]
+        public IActionResult Create(HangHoa hangHoa)
+        {
+            var newHH = new HangHoa()
+            {
+                HangHoaId = Guid.NewGuid(),
+                TenHangHoa = hangHoa.TenHangHoa,
+                DonGia = hangHoa.DonGia
+
+            };
+            hanghoas.Add(newHH);
+            return Ok(new { 
+                Success = true, Data = newHH
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, HangHoa hangHoa)
+        {
+            try
+            {
+                var hh = hanghoas.SingleOrDefault(x => x.HangHoaId == Guid.Parse(id));
+                if (hh == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    hh.DonGia = hangHoa.DonGia;
+                    hh.TenHangHoa = hangHoa.TenHangHoa;
+                    return Ok();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Demo/Asp Net Core Web API EF 5/Source Code/MyFirstWebAPI/MyFirstWebAPI/Models/HangHoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebAPI.Models
+{
+    public class HangHoa
+    {
+        [Key]
+        public Guid HangHoaId { get; set; }
+        public string TenHangHoa { get; set; }
+        public int DonGia { get; set; }
+    }
+}
